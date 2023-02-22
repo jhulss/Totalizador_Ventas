31270eeba2c21609a4ad4937e19c3503b5d024b3
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-22 06:01:15 -0400

    Primer Commit

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
 import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const cant = document.querySelector("#cant-items");
+const form = document.querySelector("#items-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  //const firstNumber = Number.parseInt(cant.value);
+  
+  div.innerHTML = "<p> Cantidad de items: " + cant.value + "</p>";
 });
