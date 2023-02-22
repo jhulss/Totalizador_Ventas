9df330b2908e1f6d8349d8f5bc9664c6eb547679
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-22 16:19:38 -0400

    Modificaciones en el presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -1,11 +1,12 @@
 import sumar from "./sumador";
 
-const cant = document.querySelector("#cantidad-num");
+const cant = document.querySelector("#cant-num");
 const form = document.querySelector("#ventas-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-    div.innerHTML = "<p> Cantidad: " + cant.value + "</p>";
+    const cantNum = Number.parseInt(cant.value);
+    div.innerHTML = "<p> Cantidad: " + cantNum.value + "</p>";
 });
