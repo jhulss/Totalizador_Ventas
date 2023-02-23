e4792fa97a8e4257d50f7b449af7344875c5c8f7
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-23 01:12:50 -0400

    Ultimas modificaciones

--- src/presenter.js
+++ src/presenter.js
@@ -4,9 +4,9 @@ import buscarEstado from "./estado";
 import buscarDescuento from "./tipoDescuento";
 import calcularDescuento from "./calcularDescuento";
 
-var precioNeto=0;
-var impuestodiv =0;
-var descuentoDiv=0;
+var precioNeto = 0;
+var impuestodiv = 0;
+var descuentoDiv = 0;
 var cantidadItems = 0;
 
 const first = document.querySelector("#primer-numero");
@@ -14,15 +14,15 @@ const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 const firstMul = document.querySelector("#primer-numeroMulti");
-const formMul =document.querySelector("#multiplicar-form");
-const divMul= document.querySelector("#resultado-divMulti");
+const formMul = document.querySelector("#multiplicar-form");
+const divMul = document.querySelector("#resultado-divMulti");
 
 const resultadoNetoForm = document.querySelector("#precioNeto-form");
 const divResNeto = document.querySelector("#resultado-divNeto");
 
-const impuestoForm =document.querySelector("#impuesto-form");
-const estadoImpuesto= document.querySelector("#impuesto-estado");
-const divImpuesto= document.querySelector("#resultado-divImpues");
+const impuestoForm = document.querySelector("#impuesto-form");
+const estadoImpuesto = document.querySelector("#impuesto-estado");
+const divImpuesto = document.querySelector("#resultado-divImpues");
 
 const descuentoForm = document.querySelector("#descuento-form");
 const divDescuento= document.querySelector("#resultado-divDescuen");
@@ -34,9 +34,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  cantidadItems =firstNumber;
+  cantidadItems = firstNumber;
 
-  div.innerHTML = "<p>" +"Cantidad de Items: "+firstNumber + "</p>";
+  div.innerHTML = "<p>" +"Cantidad de Items: "+ firstNumber + "</p>";
 });
 
 formMul.addEventListener("submit", (event) => {
@@ -44,51 +44,51 @@ formMul.addEventListener("submit", (event) => {
 
   const firstNumberMulti = Number.parseInt(firstMul.value);
 
-  divMul.innerHTML = "<p>" +"Precio por item: "+ firstNumberMulti + "</p>";
+  divMul.innerHTML = "<p>" + "Precio por item: " + firstNumberMulti + "</p>";
 });
 
 resultadoNetoForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cantItems = Number.parseInt(first.value);
-  const precioUnitario= Number.parseInt(firstMul.value);
-  precioNeto=calcularPrecioNeto(cantItems,precioUnitario);
+  const precioUnitario = Number.parseInt(firstMul.value);
+  precioNeto = calcularPrecioNeto(cantItems,precioUnitario);
 
-  divResNeto.innerHTML = "<p>" +"Precio neto (unitario x cantidad de Items): "+ precioNeto + "</p>";
+  divResNeto.innerHTML = "<p>" + "Precio neto (unitario x cantidad de Items): " + precioNeto + "</p>";
 });
 
 impuestoForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  var impu=[];
+  var impu = [];
   var impuesto = new Object();
-  impu= cargarEstados();
+  impu = cargarEstados();
   const impuestoEstado = estadoImpuesto.value;
   
   
-  impuesto= buscarEstado(impu,impuestoEstado);
+  impuesto = buscarEstado(impu,impuestoEstado);
   console.log(impuesto);
-  impuestodiv=calcularImpuesto(precioNeto,impuesto.impu);
+  impuestodiv = calcularImpuesto(precioNeto,impuesto.impu);
 
-  divImpuesto.innerHTML = "<p>" +"El impuesto para " + impuesto.name +" %:"+impuesto.imp +" es: "+impuestodiv +"</p>";
+  divImpuesto.innerHTML = "<p>" + "El impuesto para " + impuesto.name + " %:"+ impuesto.imp + " es: "+ impuestodiv + "</p>";
 });
 
 descuentoForm.addEventListener("submit", (event) => {
   event.preventDefault();
   
-  var descuento=0;
-  descuento= buscarDescuento(cantidadItems);
+  var descuento = 0;
+  descuento = buscarDescuento(cantidadItems);
   console.log(descuento);
-  descuentoDiv=calcularDescuento(precioNeto,descuento);
+  descuentoDiv = calcularDescuento(precioNeto,descuento);
 
-  divDescuento.innerHTML = "<p>" +"El Descuento para " + cantidadItems +" items es: "+descuento +"%"+"</p>";
+  divDescuento.innerHTML = "<p>" + "El Descuento para " + cantidadItems + " items es: "+descuento + "%" + "</p>";
 });
 
 totalizarForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  var respFinal=0;
-  respFinal = precioNeto+impuestodiv-descuentoDiv;
+  var respFinal = 0;
+  respFinal = precioNeto + impuestodiv - descuentoDiv;
 
-  totalizarDiv.innerHTML = "<p>" +"Precio total(descuento e impuesto): "+respFinal + "</p>";
+  totalizarDiv.innerHTML = "<p>" + "Precio total(descuento e impuesto): " + respFinal + "</p>";
 });
